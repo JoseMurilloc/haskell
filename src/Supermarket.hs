7c29dc9433bf1495615cf2097aa210cc95e9a531
Jose Murillo <jooseemurillo@gmail.com>
2022-10-03 20:28:19 -0300

    commit in class

--- src/Supermarket.hs
+++ src/Supermarket.hs
@@ -1,17 +1,3 @@
-
-
-{--
-    1) Cupom fiscal do supermercado
-Nas tarefas que se seguem temos por objetivo desenvolver uma aplicação em Haskell para
-automatizar o caixa de um supermercado usando técnicas de manipulação de listas
-empregando funções de ordem superior. Um leitor de código de barras é usado no caixa de
-um supermercado para produzir uma lista de códigos de barras a partir dos produtos que se
-encontram em um carrinho de compras contendo os produtos comprados. Usando os códigos
-de barra cria-se uma nota descritiva da compra. Considere por exemplo a seguinte lista de
-códigos de barra:
---}
-
-
 type Name = String
 type Price = Int
 type Code = Int
@@ -35,27 +21,37 @@ tableMerchandise = [
     (1234, "Dry SHERRY 2LT", 540)]
 
 
--- incrementZero :: Int -> String
--- incrementZero number
---     | number > 10  = "0" ++ number
---     | otherwise = number
-
 parseFromReal :: Int -> String
 parseFromReal value
     | value >= 10 = show (div value 100) ++ "." ++ show ( mod value 100)
     | otherwise = "0.0" ++ show (mod value 100) 
 
 
-generatorGap :: Name -> Price -> String
-generatorGap name price = replicate (sizeLine - (length name + length (parseFromReal price))) '.'
-
+generatorPointersGap :: Name -> Price -> String
+generatorPointersGap name price = replicate (sizeLine - (length name + length (parseFromReal price))) '.'
 
 formatLine :: (Name, Price) -> String;
-formatLine (name, price) = name ++ (generatorGap name price) ++ parseFromReal price
+formatLine (name, price) = name ++ (generatorPointersGap name price) ++ parseFromReal price ++ "\n"
 
 formatLines :: [(Name, Price)] -> String
 formatLines (x:[]) = formatLine x
 formatLines (x:xs) = formatLine x ++ formatLines xs
 
+
 formatTotal :: Price -> String
-formatTotal price = "\nTotal" ++ (generatorGap "Total" price) ++ parseFromReal price
+formatTotal total = formatLine ("Total", total)
+
+
+type Accounts = (Name, Price)
+formatAccount :: Accounts -> String
+formatAccount listProducts  = "Haskell store\n\n" ++ formatLines listProducts ++ formatTotal (foldl sumTotal 0 listProducts)
+    where sumTotal (name, price) acc = price + acc 
+
+
+
+
+
+
+
+
+
