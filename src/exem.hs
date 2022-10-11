afe7887181bd586da94b237ba5b806750754b6a0
José Murillo <jooseemurillo@gmail.com>
2022-10-10 22:40:19 -0300

    2) question

--- src/exem.hs
+++ src/exem.hs
@@ -30,14 +30,13 @@ cousinBetween (x:xs) n
         ['a', 'x', 'b' , 'c' ]
 --}
 
--- insertElement :: [Char] -> Char ->  Int -> [Char]
--- insertElement (x:xs) char n = [x | x <- (x:xs), x < n] ++ [n] ++ [x | x <- (x:xs), x > n]
 
 
--- insertElement :: [String] -> String -> Int -> [String]
--- insertElement (x:xs) char n = [x | x <- (x:xs), x < p] ++ [x | x <- (x:xs), x > p]
---     where p = (x:xs)!!n
-
+insertElement :: [a] -> a -> Int -> [a]
+insertElement [] newElement _ = [newElement]
+insertElement (x:xs) newElement i 
+  | i <= 0 = newElement:x:xs
+  | otherwise = x : insertElement xs newElement (i - 1)
 
 {--
     3) Defina uma função eliminarRepetidos :: [ Int ] - > [ Int ] , que recebe uma
@@ -58,36 +57,36 @@ Prelude> occurrences ["macaco", "macaco", "girafa", "zebra", "zebra",
 [("macaco",3),("girafa",2),("zebra",3)]
 --}
 
-checkAmountOccurrences :: String -> [String] -> Int
-checkAmountOccurrences name (x:xs)
-    | name == x = 1 + checkAmountOccurrences xs
-    | otherwise = 0
+-- checkAmountOccurrences :: String -> [String] -> Int
+-- checkAmountOccurrences name (x:xs)
+--     | name == x = 1 + checkAmountOccurrences xs
+--     | otherwise = 0
 
 
-occurrences :: [String] -> [Int]
-occurrences [] = []
-occurrences (x:xs) = [checkAmountOccurrences (x (x:xs)) | x <- (x:xs)]
+-- occurrences :: [String] -> [Int]
+-- occurrences [] = []
+-- occurrences (x:xs) = [checkAmountOccurrences (x (x:xs)) | x <- (x:xs)]
 
 {-
     5) Um grafo é uma estrutura de dados baseada em uma abstração matemática
-bastante popular. Grafos são usados em diferentes nichos de aplicação como
-implementação de mapas inteligentes, gerenciamento de tráfego em rede e
-interpretação de linguagens funcionais. Um grafo é um par consistindo em um
-conjunto de vértices e um conjunto de arestas. Cada aresta conecta um par de
-vértices. Diz-se que que dois vértices conectados por uma aresta são
-adjacentes. Em Haskell, é possível definir um grafo como um par:
-type Vertice = Int
-type Aresta = (Int, Int)
-type Grafo = ([Vertice], [Aresta])
-Nesta questão consideraremos grafos direcionados, ou seja, se um par (v1,
-v2) pertence à lista de arestas de um grafo, isso significa que há uma
-aresta de v1 para v2 mas não o contrário, ou seja, dizemos que v2 é um
-vizinho de v1 mas a recíproca não é verdadeira. Implemente uma função que
-recebe como entrada um grafo e produz como resultado uma lista de tuplas de
-dois elementos (V, VS), onde V é um nó do grafo VS é a lista de todos os nós
-vizinhos de V no grafo.
-Exemplo:
-Prelude> vizinhos ([1,2,3,4,5], [(1,2), (3,4), (1,4), (2, 1), (2,3), (2, 4),
-(5, 3), (4, 2)])
-[(1, [2, 4]), (3, [4]), (2, [1, 3, 4]), (5, [3]), (4, [2])]
+    bastante popular. Grafos são usados em diferentes nichos de aplicação como
+    implementação de mapas inteligentes, gerenciamento de tráfego em rede e
+    interpretação de linguagens funcionais. Um grafo é um par consistindo em um
+    conjunto de vértices e um conjunto de arestas. Cada aresta conecta um par de
+    vértices. Diz-se que que dois vértices conectados por uma aresta são
+    adjacentes. Em Haskell, é possível definir um grafo como um par:
+    type Vertice = Int
+    type Aresta = (Int, Int)
+    type Grafo = ([Vertice], [Aresta])
+    Nesta questão consideraremos grafos direcionados, ou seja, se um par (v1,
+    v2) pertence à lista de arestas de um grafo, isso significa que há uma
+    aresta de v1 para v2 mas não o contrário, ou seja, dizemos que v2 é um
+    vizinho de v1 mas a recíproca não é verdadeira. Implemente uma função que
+    recebe como entrada um grafo e produz como resultado uma lista de tuplas de
+    dois elementos (V, VS), onde V é um nó do grafo VS é a lista de todos os nós
+    vizinhos de V no grafo.
+    Exemplo:
+    Prelude> vizinhos ([1,2,3,4,5], [(1,2), (3,4), (1,4), (2, 1), (2,3), (2, 4),
+    (5, 3), (4, 2)])
+    [(1, [2, 4]), (3, [4]), (2, [1, 3, 4]), (5, [3]), (4, [2])]
 --}
